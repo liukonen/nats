075f27d531e875594101a61f0579cacc4b86fb5c
liukonen <liukonen@gmail.com>
2020-02-01 14:32:12 -0600

    again, left some debug logic in code. not cool

--- NATS/Program.cs
+++ NATS/Program.cs
@@ -19,8 +19,6 @@ namespace NATS
             string consoleOutput;
             StringBuilder FilesFound = new StringBuilder();
             string fullArgs = ArgsToArg(args);
-
-            fullArgs = @"-p c:\users\liuko\source -k luke -I";
             ArgumentsObject.ArgumentsObject ScanArg = new ArgumentsObject.ArgumentsObject(fullArgs);
             if (!ScanArg.DisplayHelp)
             {
